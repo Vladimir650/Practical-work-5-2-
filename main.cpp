9c46e8d0392444fe535fef6b631ac2e21c64d02b
Mazur Vladimir <vovamazur677@gmail.com>
2025-04-10 13:42:21 +0300

    hello

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <string> 
 
 int main(){
 
+  std::string name;
+  std::cout << "Imput yor name" << std::endl;
+  std::cin >> name;
 
+  std::cout << "Hello " << name << "!" <<std::endl;
+  
   return 0;
 }
